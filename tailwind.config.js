bc71cac9788e39fe34b8e031f52af5febbf93a13
Sebastian-Serban <81688773+Sebastian-Serban@users.noreply.github.com>
2023-12-12 21:19:09 +0100

    finish gründe.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./HTML/Index.html", ".JAVASCRIPT/script.js"],
+  content: ["./HTML/index.html", ".JAVASCRIPT/script.js"],
   theme: {
     extend: {},
   },
